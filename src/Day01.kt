9602d2ddc028e6505f5ca430b13e7688fa58361f
Jixun <8041017-jixunmoe@users.noreply.gitlab.com>
2024-12-01 22:00:26 +0000

    feat: add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,45 @@
+import kotlin.math.abs
+
 fun main() {
+    val re = Regex("^(\\d+)\\s+(\\d+)$")
     fun part1(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+        for (line in input) {
+            val m = re.find(line)
+            if (m != null) {
+                val (a, b) = m.destructured
+                left.add(a.toInt())
+                right.add(b.toInt())
+            }
+        }
+
+        return left.sorted().zip(right.sorted()).sumOf { (a, b) -> abs(a - b) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = mutableMapOf<Int, Int>()
+        val right = mutableMapOf<Int, Int>()
+        for (line in input) {
+            val m = re.find(line)
+            if (m != null) {
+                val a = m.groupValues[1].toInt()
+                val b = m.groupValues[2].toInt()
+                left[a]?.let { left[a] = it + 1 } ?: left.put(a, 1)
+                right[b]?.let { right[b] = it + 1 } ?: right.put(b, 1)
+            }
+        }
+
+        return left.keys.sumOf { k -> (k * (left[k] ?: 0) * (right[k] ?: 0)) };
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_sample")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("part1: ${part1(input)}")
+    println("part2: ${part2(input)}")
 }
